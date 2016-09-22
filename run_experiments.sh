7c1a53e0ce5477c8a8bb41483bc78c9cc85f79e3
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2016-09-22 14:39:49 -0400

    remove bash

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-bash scripts/0-data_download.sh
+scripts/0-data_download.sh
 Rscript scripts/1-get_differentially_expressed_genes.R
 Rscript scripts/2-generate_standards.R
-bash scripts/3-run_SVM.sh
+scripts/3-run_SVM.sh
 Rscript scripts/4-plot_SVM_results.R
