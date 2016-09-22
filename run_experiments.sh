3ef81ec2b0fc91308c41d0aac62d4d02854c3071
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2016-09-22 09:49:06 -0400

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,5 +3,5 @@
 bash scripts/0-data_download.sh
 Rscript scripts/1-get_differentially_expressed_genes.R
 Rscript scripts/2-generate_standards.R
-bash scripts/3-run_SVM.R
+bash scripts/3-run_SVM.sh
 Rscript scripts/4-plot_SVM_results.R
